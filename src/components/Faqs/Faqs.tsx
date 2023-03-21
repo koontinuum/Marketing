b49460c86438cefcbed3582c2f6969e77f006a1f
Joldosh123456 <usenakunov98@gmail.com>
2023-03-21 16:35:17 +0300

    fix

--- src/components/Faqs/Faqs.tsx
+++ src/components/Faqs/Faqs.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styles from "./Faqs.module.css";
 import minus from "../../assets/Faqs/minus.svg";
 import plus from "../../assets/Faqs/plus.svg";
 import faqsRight from "../../assets/Faqs/faqsRight.svg";
+import FaqsCard from "./FaqsCard/FaqsCard";
+import { Accardiondata } from "../../FaqsData";
 
 function Faqs() {
+  const render = useMemo(
+    () =>
+      Accardiondata.map((item) => (
+        <FaqsCard key={item.id} {...item} />
+      )),
+    []
+  );
   return (
     <div className="container">
       <div className={styles.faqs}>
         <div className={styles.faqsLeft}>
           <p className={styles.faqsF}>FAQS</p>
-          <p className={styles.faqsTitle}>Frequently Asked Questions</p>
-          <div className={styles.faqsDescripTop}>
+          <p className={styles.faqsTitle}>Frequently Asked <br /> Questions</p>
+          {/* <div className={styles.faqsDescripTop}>
             <p>Sed augue ipsum, egestas nec, vestibulum et</p>
             <img src={minus} alt="" />
           </div>
@@ -21,25 +30,12 @@ function Faqs() {
               vulputate arcu. Donec mi odio, faucibus at, scelerisque quis,
               convallis in,
             </p>
-          </div>
-          <div className={styles.faqsDescripAll}>
-            <p>
-              Malesuada adipiscing, dui vestibulum suscipit nulla quis orci.
-            </p>
-            <img src={plus} alt="" />
-          </div>
-          <div className={styles.faqsDescripAll}>
-            <p>Ut varius tincidunt libero.</p>
-            <img src={plus} alt="" />
-          </div>
-          <div className={styles.faqsDescripAll}>
-            <p>In ut quam vitae odio lacinia tincidunt.</p>
-            <img src={plus} alt="" />
-          </div>
-          <div className={styles.faqsDescripAll}>
-            <p>Fusce vel dui Morbi nec metus.</p>
-            <img src={plus} alt="" />
-          </div>
+          </div> */}
+          
+        <>{render}</>          
+         <div>
+        
+         </div>
         </div>
         <div className={styles.faqsRight}>
           <img src={faqsRight} alt="" />
