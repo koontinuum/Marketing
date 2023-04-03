8d335c1c602318ea07a173b38e03e1ef010fcb04
Joldosh123456 <usenakunov98@gmail.com>
2023-04-03 18:38:40 +0300

    slidersList

--- src/Pages/SlidersPage/SlidersPage.tsx
+++ src/Pages/SlidersPage/SlidersPage.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import BigSlider from "../../components/BigSlider/BigSlider";
 import Categories from "../../components/Categories/Categories";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import SlidersList from "../../components/SlidersList/SlidersList";
+import UpFooter from "../../components/UpFooter/UpFooter";
+import SeoAnalysis from "../../components/SeoAnalysis/SeoAnalysis";
 
 function SlidersPage() {
   return (
     <>
       <Header />
       <BigSlider />
-      <SlidersList/>
+      <SlidersList />
+      <SeoAnalysis />
+      <UpFooter />
+      <Footer />
     </>
   );
 }
--- src/SlidersItems.ts
+++ src/SlidersItems.ts
@@ -1,5 +1,29 @@
 import franBack from "../src/assets/SlidersList/franBack.svg";
 import franAva from "../src/assets/SlidersList/franAva.svg";
+import antAva from "../src/assets/SlidersList/antAva.svg";
+import antBack from "../src/assets/SlidersList/antBack.svg";
+import bianAva from "../src/assets/SlidersList/bianAva.svg";
+import bianBack from "../src/assets/SlidersList/bianBack.svg";
+import calAva from "../src/assets/SlidersList/calAva.svg";
+import calBack from "../src/assets/SlidersList/calBack.svg";
+import husAva from "../src/assets/SlidersList/husAva.svg";
+import husBack from "../src/assets/SlidersList/husBack.svg";
+import madAva from "../src/assets/SlidersList/madAva.svg";
+import marAva from "../src/assets/SlidersList/marAva.svg";
+import madBack from "../src/assets/SlidersList/madBack.svg";
+import marBack from "../src/assets/SlidersList/marBack.svg";
+import pamAva from "../src/assets/SlidersList/pamAva.svg";
+import pamBack from "../src/assets/SlidersList/pamBack.svg";
+
+
+
+
+
+
+
+
+
+
 import dot from "../src/assets/SlidersList/ellips.svg";
 
 export const ItemData = [
@@ -15,72 +39,72 @@ export const ItemData = [
   },
   {
     id: 2,
-    img: franBack,
+    img: bianBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: bianAva,
+    userName: "Bianka Lancaster",
     dot: dot,
   },
   {
-    id: 2,
-    img: franBack,
+    id: 3,
+    img: husBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: husAva,
+    userName: "Husna Navarro",
     dot: dot,
   },
   {
-    id: 2,
-    img: franBack,
+    id: 4,
+    img: madBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: marAva,
+    userName: "Maddox Fletcher",
     dot: dot,
   },
   {
-    id: 2,
-    img: franBack,
+    id: 5,
+    img: calBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: calAva,
+    userName: "Calista Campbell",
     dot: dot,
   },
   {
-    id: 2,
-    img: franBack,
+    id: 6,
+    img: antBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: antAva,
+    userName: "Antoine Findlay",
     dot: dot,
   },
   {
-    id: 2,
-    img: franBack,
+    id: 7,
+    img: marBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: marAva,
+    userName: "Margaret Donnelly",
     dot: dot,
   },
   {
-    id: 2,
-    img: franBack,
+    id: 8,
+    img: antBack,
     title1: "24 May 2019",
     title2: "8 min read",
     desc: "How to design a product that can grow itself 10x in year:",
-    userImg: franAva,
-    userName: "Francesca Richards",
+    userImg: antAva,
+    userName: "Pamela Mclellan",
     dot: dot,
   },
 ];
